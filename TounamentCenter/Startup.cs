2a7f659c8041213e66e20bd8e3ef6b227080eaa7
OldBen <krzysztof.buniewicz@gmail.com>
2016-06-09 13:48:33 +0200

    Initial commit

---
+++ TounamentCenter/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TounamentCenter.Startup))]
+namespace TounamentCenter
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
