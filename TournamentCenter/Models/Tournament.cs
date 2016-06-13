7b33f6b6d633d8ba25c65a07ba74fa99ae144bff
OldBen <krzysztof.buniewicz@gmail.com>
2016-06-13 13:55:00 +0200

    fixed column name display

--- TournamentCenter/Models/Tournament.cs
+++ TournamentCenter/Models/Tournament.cs
@@ -20,11 +20,14 @@ namespace TournamentCenter.Models
             set { id = value; }
         }
 
+        [Display(Name = "Tournament Name", Description = "Name of the event.")]
         public string TournamentName
         {
             get { return name; }
             set { name = value; }
         }
+
+        [Display(Name = "Date", Description = "Date of the event.")]
         [DataType(DataType.Date)]
         public DateTime Date
         {
@@ -32,13 +35,14 @@ namespace TournamentCenter.Models
             set { date = value; }
         }
 
-
+        [Display(Name = "Location", Description = "Place where the event will take place.")]
         public string Location
         {
             get { return location; }
             set { location = value; }
         }
 
+        [Display(Name = "Participants Limit", Description = "Maximum number of players that can compete in event.")]
         public int ParticipantsLimit
         {
             get { return limit; }
